5714b71a04d40382eaedd178f5b4a442fe328989
roko02 <90667780+ROKO02@users.noreply.github.com>
2025-01-14 16:05:58 +0900

    기초적인 기능
    추가 및 삭제 기능

---
+++ ToDoList/App.js
@@ -0,0 +1,55 @@
+import { useState } from "react";
+
+function App() {
+  const [inputVlaue, setInputValue] = useState("");
+  const [inputList, setInputList] = useState([]);
+
+  const changeInput = (e) => {
+    setInputValue(e.target.value);
+  };
+
+  const addList = () => {
+    setInputList([...inputList, inputVlaue]);
+    setInputValue("");
+  };
+
+  const DelteList = (index) => {
+    setInputList(
+      inputList.filter((item, i) => {
+        return index !== i;
+      })
+    );
+  };
+
+  return (
+    <div>
+      <div>
+        <input type="input" value={inputVlaue} onChange={changeInput} />
+        <button type="button" onClick={addList}>
+          확인
+        </button>
+      </div>
+      <div>
+        <ul>
+          {inputList.map((item, i) => {
+            return (
+              <li>
+                <span>{item}</span>
+                <button
+                  type="button"
+                  onClick={(e) => {
+                    DelteList(i);
+                  }}
+                >
+                  삭제
+                </button>
+              </li>
+            );
+          })}
+        </ul>
+      </div>
+    </div>
+  );
+}
+
+export default App;
